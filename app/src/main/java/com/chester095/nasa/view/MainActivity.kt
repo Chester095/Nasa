535281b6b86310faf6f6f75572d9d2637fe9089b
Chester095 <Chester095@users.noreply.github.com>
2022-02-27 17:05:53 +0300

    HW2 откатил слегка и пока рабоют только стили

--- app/src/main/java/com/chester095/nasa/view/MainActivity.kt
+++ app/src/main/java/com/chester095/nasa/view/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.chester095.nasa.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.TextView
 import com.chester095.nasa.R
 import com.chester095.nasa.view.main.MainFragment
 
+const val PREFS_NAME = "theme_prefs"
+const val KEY_THEME = "prefs.theme"
+const val KEY_SP = "sp"
+const val KEY_CURRENT_THEME = "current_theme"
+const val THEME_UNDEFINED = -1
+const val THEME_LIGHT = 0
+const val THEME_DARK = 1
+const val THEME_SYSTEM = 2
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // SP
-        setTheme(R.style.MyThemeIndigo)
+        setTheme(getCurrentTheme())
+        Log.d("!!!", ""+getCurrentTheme())
         setContentView(R.layout.activity_main)
 
-        TextView(this).apply {
-            text=""
-            textSize=30f
-            gravity = Gravity.NO_GRAVITY
-
-        }
 
         if(savedInstanceState==null){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container,MainFragment.newInstance()).commit()
         }
-
-//        recreate()
     }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+
 }
--- app/src/main/java/com/chester095/nasa/view/StylesFragment.kt
+++ app/src/main/java/com/chester095/nasa/view/StylesFragment.kt
@@ -1,5 +1,6 @@
 package com.chester095.nasa.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -22,31 +23,96 @@ class StylesFragment : Fragment() {
     val binding: FragmentStylesBinding
         get() = _binding!!
 
+    private lateinit var parentActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = requireActivity() as MainActivity
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStylesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+/*        initTheme()
+        initThemeListener()*/
+        initBtnStyle()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
+    private fun initBtnStyle() {
+        binding.btnOrange.setOnClickListener {
+            if (parentActivity.getCurrentTheme() != R.style.MyThemeOrange) {
+                parentActivity.setCurrentTheme(R.style.MyThemeOrange)
+                parentActivity.recreate()
+            }
+        }
+        binding.btnBlueGray.setOnClickListener {
+            if (parentActivity.getCurrentTheme() != R.style.MyThemeBlueGray) {
+                parentActivity.setCurrentTheme(R.style.MyThemeBlueGray)
+                parentActivity.recreate()
+            }
+        }
+        binding.btnIndigo.setOnClickListener {
+            if (parentActivity.getCurrentTheme() != R.style.MyThemeIndigo) {
+                parentActivity.setCurrentTheme(R.style.MyThemeIndigo)
+                parentActivity.recreate()
+            }
+        }
+    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab!!.position) {
+/*    private fun initTheme() {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB -> {
+                binding.themeSystem.visibility = View.VISIBLE
+            }
+            else -> {
+                binding.themeSystem.visibility = View.GONE
+            }
+        }
+        when {
+            getSavedTheme() == THEME_LIGHT -> binding.themeLight.isChecked = true
+            getSavedTheme() == THEME_DARK -> binding.themeDark.isChecked = true
+            getSavedTheme() == THEME_SYSTEM -> binding.themeSystem.isChecked = true
+            getSavedTheme() == THEME_UNDEFINED -> {
+                when {
+                    resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) ==
+                            Configuration.UI_MODE_NIGHT_NO -> binding.themeLight.isChecked = true
+                    resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) ==
+                            Configuration.UI_MODE_NIGHT_YES -> binding.themeDark.isChecked = true
+                    resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK) ==
+                            Configuration.UI_MODE_NIGHT_UNDEFINED -> binding.themeLight.isChecked = true
                 }
             }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
+        }
     }
+
+    private fun initThemeListener() {
+        binding.themeGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.themeLight -> setTheme(AppCompatDelegate.MODE_NIGHT_NO, THEME_LIGHT)
+                R.id.themeDark -> setTheme(AppCompatDelegate.MODE_NIGHT_YES, THEME_DARK)
+                R.id.themeSystem -> setTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, THEME_SYSTEM)
+            }
+        }
+    }
+
+    private fun setTheme(themeMode: Int, prefsMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        saveTheme(prefsMode)
+    }
+
+    private fun saveTheme(theme: Int) = parentActivity.sharedPrefs.edit().putInt(KEY_THEME, theme).apply()
+    private fun getSavedTheme() = parentActivity.sharedPrefs.getInt(KEY_THEME, THEME_UNDEFINED)*/
 }
